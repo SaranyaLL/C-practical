482b2a56c822b66c37e12c6d653bd5b778e10f48
SaranyaLL <112744611+SaranyaLL@users.noreply.github.com>
2022-10-08 13:24:24 +0530

    Add files via upload

---
+++ D3 programming.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<"enter the of a =";
+	cin>>a;
+	cout<<"enter the of b =";
+	cin>>b;
+	c=a-b;
+	cout<<"the sub = "<<c;
+}
